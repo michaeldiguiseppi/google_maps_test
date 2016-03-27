fafe63cc7aaef621668caf3025e8acbf1ae29060
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-03-27 10:24:06 -0600

    Working on making directions line work while still using back-end page reload.

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -2,58 +2,18 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
-  navigator.geolocation.getCurrentPosition(success);
-});
-
-$('#submit').on('click', function(event) {
-  event.preventDefault();
-  drawMap();
-});
-
-function drawMap() {
-  var start = $('#start').val();
-  var end = $('#end').val();
-
-  var options = { method: 'GET',
-    url: 'https://maps.googleapis.com/maps/api/directions/json',
-    qs:
-     { origin: start,
-       destination: end,
-       key: process.env.MAPS_API_KEY }
-    };
-
-  $.ajax(options).done(function(result) {
-    console.log(result);
+  navigator.geolocation.getCurrentPosition(function(pos) {
+      var lat = pos.coords.latitude;
+      var long = pos.coords.longitude;
   });
-
-  // request(options, function (error, response, body) {
-  //   if (error) throw new Error(error);
-  //
-  //   console.log(JSON.parse(body));
-  //   var steps = JSON.parse(body).routes[0].legs[0];
-  //   //res.json(JSON.parse(body));
-  //   res.render('index', {steps: steps.steps, duration: steps.duration.text, distance: steps.distance.text, start: steps.start_address, end: steps.end_address, api_key: process.env.MAPS_API_KEY});
-  // });
-}
-
-
-
-
-var lat, long;
-
-function success(pos) {
-  var crd = pos.coords;
-
-  lat = crd.latitude;
-  long = crd.longitude;
-}
+});
 
 function initMap() {
     var directionsService = new google.maps.DirectionsService;
     var directionsDisplay = new google.maps.DirectionsRenderer;
 
     var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 7,
+      zoom: 10,
     });
     navigator.geolocation.getCurrentPosition(function(position) {
       initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
@@ -66,16 +26,16 @@ function initMap() {
     document.getElementById('submit').addEventListener('click', onChangeHandler);
   }
 
-      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-        directionsService.route({
-          origin: document.getElementById('start').value,
-          destination: document.getElementById('end').value,
-          travelMode: google.maps.TravelMode.DRIVING
-        }, function(response, status) {
-          if (status === google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-      }
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: document.getElementById('start').value,
+    destination: document.getElementById('end').value,
+    travelMode: google.maps.TravelMode.DRIVING
+  }, function(response, status) {
+    if (status === google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
